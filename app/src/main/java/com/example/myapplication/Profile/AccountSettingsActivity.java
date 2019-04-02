181662212c25ca1cf0adf18397b632ec7c05365c
shubhamsawantsjsu <shubham.sawant@sjsu.edu>
2019-04-02 11:09:47 -0700

    Added fragments to account settings page

--- app/src/main/java/com/example/myapplication/Profile/AccountSettingsActivity.java
+++ app/src/main/java/com/example/myapplication/Profile/AccountSettingsActivity.java
@@ -1,17 +1,20 @@
 package com.example.myapplication.Profile;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.example.myapplication.R;
+import com.example.myapplication.Utils.SectionsStatePagerAdapter;
 
 import java.util.ArrayList;
 
@@ -21,16 +24,24 @@ public class AccountSettingsActivity extends AppCompatActivity {
 
     private static final String TAG = "AccountSettingsActivity";
 
+    private SectionsStatePagerAdapter pagerAdapter;
+    private ViewPager mViewPager;
+    private RelativeLayout mRelativeLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountsettings);
         Log.d(TAG, "onCreate: started");
+        mViewPager = findViewById(R.id.container);
+        mRelativeLayout = findViewById(R.id.relLayout1);
 
         setupSettingsList();
 
-        ImageView backArrow = findViewById(R.id.backArrow);
+        setUpFragments();
 
+        //setup the backarrow for navigating back to "ProfileActivity"
+        ImageView backArrow = findViewById(R.id.backArrow);
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,15 +51,37 @@ public class AccountSettingsActivity extends AppCompatActivity {
         });
     }
 
+    private void setUpFragments() {
+        pagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new EditProfileFragment(), getString(R.string.edit_profile_fragment));
+        pagerAdapter.addFragment(new SignOutFragment(), getString(R.string.sign_out_fragment));
+    }
+
+    private void setViewPager(int fragmentNumber) {
+
+        mRelativeLayout.setVisibility(View.GONE);
+        Log.d(TAG, "setViewPager: navigating to fragment #: " + fragmentNumber);
+        mViewPager.setAdapter(pagerAdapter);
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
     private void setupSettingsList() {
         Log.d(TAG, "setupSettingsList: initializing 'Account Settings' list");
 
         ListView listView = findViewById(R.id.lvAccountSettings);
         ArrayList<String> options = new ArrayList<>();
-        options.add(getString(R.string.edit_profile));
-        options.add(getString(R.string.sign_out));
+        options.add(getString(R.string.edit_profile_fragment));
+        options.add(getString(R.string.sign_out_fragment));
 
         ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: navigating to fragment #:" + position);
+                setViewPager(position);
+            }
+        });
     }
 }
